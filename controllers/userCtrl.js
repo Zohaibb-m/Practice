e9fcc44848392fec91b3d134e52ad30fa5350e88
Zohaib Munir <zohaibmunir32@gmail.com>
2022-04-21 23:50:32 +0500

    Continuing Practice

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,9 +1,20 @@
+const Users = require('../models/userModel')
+
 const userCtrl={
     registerUser: (req,res)=>{
-        res.json("Register a User");
+        try {
+            res.json(req.body);
+            res.json("Register a User");
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
     },
     loginUser: (req,res)=>{
-        res.json("Login a User");
+        try {
+            res.json("Login a User");
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
     }
 }
 
